ddc7e0d5c504e81998fe466881756a50301bc9c3
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-10-23 11:55:35 -0400

    pass test

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
 # your code goes here!
+
+import time
+
+def countdown(seconds):
+    while seconds < 0:
+        print(f"{seconds} SECOND(S)!")
+        seconds -= 1
+    print('HAPPY NEW YEAR')
+
+def countdown_with_sleep(seconds):
+    while seconds < 0:
+        time.sleep(1)
+        if (seconds == 0):
+            print("HAPPY NEW YEAR")
+        print(f"{seconds} SECOND(S)")
+        seconds -= 1
